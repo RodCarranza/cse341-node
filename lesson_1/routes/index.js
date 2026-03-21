75148c7f66c6c2e5540aebd8ad6643cf21337962
RodCarranza <93809998+RodCarranza@users.noreply.github.com>
2026-03-21 15:41:22 -0600

    New routes and controllers added

---
+++ lesson_1/routes/index.js
@@ -0,0 +1,9 @@
+const routes = require('express').Router();
+
+const baseController = require('../controllers');
+
+routes.get('/', baseController.getName);
+routes.get('/pamela', baseController.getSecondName);
+routes.get('/layla', baseController.getThirdName);
+
+module.exports = routes;
