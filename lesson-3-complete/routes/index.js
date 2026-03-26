3ff690ec7b9635f62b82a4bd733565fbaf882273
Tester <tester@local.dev>
2026-03-26 15:59:33 -0600

    Rename folder project

---
+++ lesson-3-complete/routes/index.js
@@ -0,0 +1,9 @@
+const routes = require('express').Router();
+const contactsRouter = require('./contacts'); // import the contacts routes
+
+// Mount the contacts routes under the /contacts path
+routes.use('/contacts', contactsRouter);
+
+module.exports = routes;
+
+
