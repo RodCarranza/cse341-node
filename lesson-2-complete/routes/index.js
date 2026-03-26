6c17c5e9754f3c302e91951be8b68aa10766cf87
Tester <tester@local.dev>
2026-03-26 16:17:02 -0600

    Remove lesson-2 folder and add lesson-3 folder

--- lesson-2-complete/routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-const contactsRouter = require('./contacts'); // import the contacts routes
-
-// Mount the contacts routes under the /contacts path
-routes.use('/contacts', contactsRouter);
-
-module.exports = routes;
-
-
