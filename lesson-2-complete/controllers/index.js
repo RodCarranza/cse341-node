60c793922b4a0047a4e07ad9c1fa250fb458f17f
Tester <tester@local.dev>
2026-03-26 15:43:33 -0600

    personal assignment L03 working locally

--- lesson-2-complete/controllers/index.js
+++ lesson-2-complete/controllers/index.js
@@ -1,13 +1,3 @@
-const getName = (req, res) => {
-  res.send('Sarah Birch');
-};
 
-const getSecondName = (req, res) => {
-  res.send('Pamela Gomez');
-};
 
-const getThirdName = (req, res) => {
-  res.send('Layla Lopez');
-};
-
-module.exports = { getName, getSecondName, getThirdName};
+module.exports = {};
--- lesson-2-complete/routes/index.js
+++ lesson-2-complete/routes/index.js
@@ -1,17 +1,9 @@
-
 const routes = require('express').Router();
-const baseController = require('../controllers');
 const contactsRouter = require('./contacts'); // import the contacts routes
 
-routes.get('/', baseController.getName);
-routes.get('/pamela', baseController.getSecondName);
-routes.get('/layla', baseController.getThirdName);
-
-// new contacts routes
-routes.use('/contacts', contactsRouter); // mount under /contacts
+// Mount the contacts routes under the /contacts path
+routes.use('/contacts', contactsRouter);
 
 module.exports = routes;
-
-//
 
 
