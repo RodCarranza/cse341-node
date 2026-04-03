fb66d20b4b97844ce921839f379adae419b985f6
RodCarranza <93809998+RodCarranza@users.noreply.github.com>
2026-04-02 22:40:04 -0600

    Final modifications to make project work on localhost

--- lesson-4-complete/routes/index.js
+++ lesson-4-complete/routes/index.js
@@ -1,5 +1,6 @@
 const routes = require('express').Router();
 const contactsRouter = require('./contacts'); // import the contacts routes
+const router = require('./swagger');
 
 router.use('/', require('./swagger'));
 // Mount the contacts routes under the /contacts path
--- lesson-4-complete/server.js
+++ lesson-4-complete/server.js
@@ -3,8 +3,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+// Swagger setup
+const swaggerUi = require('swagger-ui-express');
+const swaggerDocument = require('./swagger.json');
+
 // Middleware to parse JSON
 app.use(express.json());
+
+// ADDED BEFORE YOUR ROUTES
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // MongoDB connection
 const { connectDB } = require('./db/connect');
