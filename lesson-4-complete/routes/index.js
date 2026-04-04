1c25c52006e2fa363efbfbc2809a456bdbe12c73
RodCarranza <93809998+RodCarranza@users.noreply.github.com>
2026-04-03 23:43:36 -0600

    Delete unnecessary files

--- lesson-4-complete/routes/index.js
+++
@@ -1,9 +0,0 @@
-const routes = require('express').Router();
-const contactsRouter = require('./contacts'); // import the contacts routes
-const router = require('./swagger');
-
-router.use('/', require('./swagger'));
-// Mount the contacts routes under the /contacts path
-routes.use('/contacts', contactsRouter);
-
-module.exports = routes;
