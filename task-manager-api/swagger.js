abc135d5725d16f3c3c2caece378bd51b2497b19
RodCarranza <93809998+RodCarranza@users.noreply.github.com>
2026-04-05 22:20:46 -0600

    Change localhost to Render URL for production environment

--- task-manager-api/swagger.js
+++ task-manager-api/swagger.js
@@ -5,9 +5,9 @@ const doc = {
     title: 'Task Manager API',
     description: 'API for managing tasks and users'
   },
-  host: 'localhost:3000',
-  //host: 'cse341-node-5.onrender.com',
-  schemes: ['http'],
+  //host: 'localhost:3000',
+  host: 'cse341-node-6.onrender.com',
+  schemes: ['https'],
   tags: [
     {
       name: 'TASKS',
